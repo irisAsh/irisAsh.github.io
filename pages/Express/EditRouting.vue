1bc2aa17114d87d5f491296a018e3784ad033a2a
irisAsh <043takotoka@gmail.com>
2019-03-07 00:37:23 +0900

    Add some pages

---
+++ pages/Express/EditRouting.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="wrapper">
+    <article id="main">
+      <section>
+        <div v-html="EditRoutingMd"></div>
+      </section>
+    </article>
+    <side-menu :menuSections="menuSections"/>
+  </div>
+</template>
+
+<script>
+import SideMenu from '@/components/SideMenu'
+import ExpressMenu from '@/assets/json/express_menu.json'
+import EditRoutingMd from '@/static/markdown/express/edit_routing.md'
+
+export default {
+  components: {
+    SideMenu
+  },
+  data: () => ({
+    menuSections: ExpressMenu.menuItems
+  }),
+  computed: {
+    EditRoutingMd() {
+      return EditRoutingMd
+    }
+  }
+}
+</script>
---
+++ pages/Express/Links.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="wrapper">
+    <article id="main">
+      <section>
+        <div v-html="LinkMd"></div>
+      </section>
+    </article>
+    <side-menu :menuSections="menuSections"/>
+  </div>
+</template>
+
+<script>
+import SideMenu from '@/components/SideMenu'
+import ExpressMenu from '@/assets/json/express_menu.json'
+import LinkMd from '@/static/markdown/express/links.md'
+
+export default {
+  components: {
+    SideMenu
+  },
+  data: () => ({
+    menuSections: ExpressMenu.menuItems
+  }),
+  computed: {
+    LinkMd() {
+      return LinkMd
+    }
+  }
+}
+</script>

811850e619a1f8632103a3524c2e3aa9afb175c6
irisAsh <043takotoka@gmail.com>
2019-04-28 01:52:48 +0900

    Add session page and thirdparty page

--- script/c_ex_page.sh
+++ script/c_ex_page.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/zsh
 
 cd /Users/hirokikojima/Project/github-pages
-page_name=$1
-cp script/templates/express_template.txt "pages/express/${page_name}.vue"
-touch "static/markdown/express/${page_name}.md"
+directory_name=$1
+page_name=$2
+cp script/templates/express_template.txt "pages/${directory_name}/${page_name}.vue"
+touch "static/markdown/${directory_name}/${page_name}.md"

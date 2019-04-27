2733151c999c8a2122819dc4b3d6c93546033104
irisAsh <043takotoka@gmail.com>
2019-04-27 21:23:49 +0900

    Add Redis setup page

---
+++ pages/redis/index.vue
@@ -0,0 +1,448 @@
+<template>
+  <div>
+    <section id="express-header">
+      <header>
+        <h1 class="title">
+          Redis
+        </h1>
+      </header>
+      <p class="subtitle">
+      	キー・バリュー型のインメモリデータベース
+      </p>
+      <div class="redis-animation-container">
+        <svg
+          xmlns:dc="http://purl.org/dc/elements/1.1/"
+          xmlns:cc="http://creativecommons.org/ns#"
+          xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+          xmlns:svg="http://www.w3.org/2000/svg"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+          xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+          width="100%"
+          height="200px"
+          viewBox="0 0 190.5 184.15"
+          version="1.1"
+          id="svg8"
+          inkscape:version="0.92.4 (5da689c313, 2019-01-14)"
+          sodipodi:docname="redis-mark.svg">
+          <defs
+            id="defs2">
+            <inkscape:perspective
+              sodipodi:type="inkscape:persp3d"
+              inkscape:vp_x="18.174326 : 145.29276 : 1"
+              inkscape:vp_y="0 : 999.99995 : 0"
+              inkscape:vp_z="204.12008 : 147.96545 : 1"
+              inkscape:persp3d-origin="138.14142 : 85.63652 : 1"
+              id="perspective4522" />
+          </defs>
+          <sodipodi:namedview
+            id="base"
+            pagecolor="#ffffff"
+            bordercolor="#666666"
+            borderopacity="1.0"
+            inkscape:pageopacity="0.0"
+            inkscape:pageshadow="2"
+            inkscape:zoom="0.49497475"
+            inkscape:cx="468.06663"
+            inkscape:cy="782.6697"
+            inkscape:document-units="px"
+            inkscape:current-layer="layer1"
+            showgrid="false"
+            units="in"
+            inkscape:snap-global="false"
+            inkscape:window-width="1011"
+            inkscape:window-height="1048"
+            inkscape:window-x="-3"
+            inkscape:window-y="0"
+            inkscape:window-maximized="0"
+            width="9.5in" />
+          <metadata
+            id="metadata5">
+            <rdf:RDF>
+              <cc:Work
+                rdf:about="">
+                <dc:format>image/svg+xml</dc:format>
+                <dc:type
+                  rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                <dc:title></dc:title>
+              </cc:Work>
+            </rdf:RDF>
+          </metadata>
+          <g
+            inkscape:label="Layer 1"
+            inkscape:groupmode="layer"
+            id="layer1"
+            transform="translate(0,-112.84996)">
+            <g
+              transform="translate(-28.33057,-6.4144631)"
+              id="g4702">
+              <g
+                id="g4698"
+                transform="translate(0,128.13393)">
+                <rect
+                  style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:9.99999905;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                  id="rect4690"
+                  width="27.882462"
+                  height="16.764587"
+                  x="186.95114"
+                  y="98.114365" />
+                <rect
+                  style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:10;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                  id="rect4692"
+                  width="23.277964"
+                  height="17.386904"
+                  x="32.192959"
+                  y="105.72569" />
+                <g
+                  id="g4696">
+                  <path
+                    inkscape:transform-center-x="-2.1778549e-06"
+                    transform="matrix(0.8660254,0.5,-1,0.57735027,239.7766,-72.159288)"
+                    sodipodi:type="star"
+                    style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:#8e0000;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                    id="path4694"
+                    sodipodi:sides="4"
+                    sodipodi:cx="123.47852"
+                    sodipodi:cy="223.23361"
+                    sodipodi:r1="64.26001"
+                    sodipodi:r2="45.43869"
+                    sodipodi:arg1="0.78539816"
+                    sodipodi:arg2="1.5707963"
+                    inkscape:flatsided="false"
+                    inkscape:rounded="-1.70003e-16"
+                    inkscape:randomized="0"
+                    d="m 168.91721,268.6723 -45.43869,0 -45.438685,0 -2e-6,-45.43869 10e-7,-45.43869 45.438686,0 45.43869,0 0,45.43869 z"
+                    inkscape:transform-center-y="1.6528791e-06" />
+                </g>
+              </g>
+              <path
+                inkscape:transform-center-x="-2.1778549e-06"
+                transform="matrix(0.8660254,0.5,-1,0.57735027,239.7766,39.024835)"
+                sodipodi:type="star"
+                style="opacity:1;fill:#e80f0f;fill-opacity:1;fill-rule:nonzero;stroke:#e80f0f;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                id="path4700"
+                sodipodi:sides="4"
+                sodipodi:cx="123.47852"
+                sodipodi:cy="223.23361"
+                sodipodi:r1="64.26001"
+                sodipodi:r2="45.43869"
+                sodipodi:arg1="0.78539816"
+                sodipodi:arg2="1.5707963"
+                inkscape:flatsided="false"
+                inkscape:rounded="-1.70003e-16"
+                inkscape:randomized="0"
+                d="m 168.91721,268.6723 -45.43869,0 -45.438685,0 -2e-6,-45.43869 10e-7,-45.43869 45.438686,0 45.43869,0 0,45.43869 z"
+                inkscape:transform-center-y="1.6528791e-06" />
+            </g>
+            <g
+              id="g4688"
+              transform="translate(-28.33057,-33.141416)">
+              <g
+                transform="translate(0,128.13393)"
+                id="g4665">
+                <rect
+                  y="98.114365"
+                  x="186.95114"
+                  height="16.764587"
+                  width="27.882462"
+                  id="rect4657"
+                  style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:9.99999905;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal" />
+                <rect
+                  y="105.72569"
+                  x="32.192959"
+                  height="17.386904"
+                  width="23.277964"
+                  id="rect4659"
+                  style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:10;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal" />
+                <g
+                  id="g4663">
+                  <path
+                    inkscape:transform-center-y="1.6528791e-06"
+                    d="m 168.91721,268.6723 -45.43869,0 -45.438685,0 -2e-6,-45.43869 10e-7,-45.43869 45.438686,0 45.43869,0 0,45.43869 z"
+                    inkscape:randomized="0"
+                    inkscape:rounded="-1.70003e-16"
+                    inkscape:flatsided="false"
+                    sodipodi:arg2="1.5707963"
+                    sodipodi:arg1="0.78539816"
+                    sodipodi:r2="45.43869"
+                    sodipodi:r1="64.26001"
+                    sodipodi:cy="223.23361"
+                    sodipodi:cx="123.47852"
+                    sodipodi:sides="4"
+                    id="path4661"
+                    style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:#8e0000;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                    sodipodi:type="star"
+                    transform="matrix(0.8660254,0.5,-1,0.57735027,239.7766,-72.159288)"
+                    inkscape:transform-center-x="-2.1778549e-06" />
+                </g>
+              </g>
+              <path
+                inkscape:transform-center-y="1.6528791e-06"
+                d="m 168.91721,268.6723 -45.43869,0 -45.438685,0 -2e-6,-45.43869 10e-7,-45.43869 45.438686,0 45.43869,0 0,45.43869 z"
+                inkscape:randomized="0"
+                inkscape:rounded="-1.70003e-16"
+                inkscape:flatsided="false"
+                sodipodi:arg2="1.5707963"
+                sodipodi:arg1="0.78539816"
+                sodipodi:r2="45.43869"
+                sodipodi:r1="64.26001"
+                sodipodi:cy="223.23361"
+                sodipodi:cx="123.47852"
+                sodipodi:sides="4"
+                id="path4634"
+                style="opacity:1;fill:#e80f0f;fill-opacity:1;fill-rule:nonzero;stroke:#e80f0f;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                sodipodi:type="star"
+                transform="matrix(0.8660254,0.5,-1,0.57735027,239.7766,39.024835)"
+                inkscape:transform-center-x="-2.1778549e-06" />
+            </g>
+            <g
+              id="g4680"
+              transform="translate(-28.33057,67.886463)">
+              <g
+                id="g4655">
+                <rect
+                  style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:9.99999905;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                  id="rect4642"
+                  width="27.882462"
+                  height="16.764587"
+                  x="186.95114"
+                  y="98.114365" />
+                <rect
+                  style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:10;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                  id="rect4640"
+                  width="23.277964"
+                  height="17.386904"
+                  x="32.192959"
+                  y="105.72569" />
+                <g
+                  id="g4645">
+                  <path
+                    inkscape:transform-center-x="-2.1778549e-06"
+                    transform="matrix(0.8660254,0.5,-1,0.57735027,239.7766,-72.159288)"
+                    sodipodi:type="star"
+                    style="opacity:1;fill:#8e0000;fill-opacity:1;fill-rule:nonzero;stroke:#8e0000;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                    id="path4616"
+                    sodipodi:sides="4"
+                    sodipodi:cx="123.47852"
+                    sodipodi:cy="223.23361"
+                    sodipodi:r1="64.26001"
+                    sodipodi:r2="45.43869"
+                    sodipodi:arg1="0.78539816"
+                    sodipodi:arg2="1.5707963"
+                    inkscape:flatsided="false"
+                    inkscape:rounded="-1.70003e-16"
+                    inkscape:randomized="0"
+                    d="m 168.91721,268.6723 -45.43869,0 -45.438685,0 -2e-6,-45.43869 10e-7,-45.43869 45.438686,0 45.43869,0 0,45.43869 z"
+                    inkscape:transform-center-y="1.6528791e-06" />
+                </g>
+              </g>
+              <g
+                transform="matrix(1,0,-0.57735027,1,59.136706,0)"
+                id="g4624">
+                <g
+                  transform="matrix(1.1547005,0.5,-0.66666667,0.57735027,49.182988,-18.448189)"
+                  id="g4632">
+                  <path
+                    d="m 168.91721,147.86647 -45.43869,0 -45.438685,0 -2e-6,-45.43869 10e-7,-45.438688 45.438686,-2e-6 45.43869,1e-6 0,45.438689 z"
+                    inkscape:randomized="0"
+                    inkscape:rounded="-1.70003e-16"
+                    inkscape:flatsided="false"
+                    sodipodi:arg2="1.5707963"
+                    sodipodi:arg1="0.78539816"
+                    sodipodi:r2="45.43869"
+                    sodipodi:r1="64.26001"
+                    sodipodi:cy="102.42778"
+                    sodipodi:cx="123.47852"
+                    sodipodi:sides="4"
+                    id="path4520"
+                    style="opacity:1;fill:#e80f0f;fill-opacity:1;fill-rule:nonzero;stroke:#e80f0f;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
+                    sodipodi:type="star"
+                    inkscape:transform-center-y="-1.1100033e-06" />
+                  <path
+                    transform="matrix(0.33938067,-0.04037272,0.04037272,0.33938067,50.56795,49.39901)"
+                    inkscape:transform-center-y="0.013061786"
+                    inkscape:transform-center-x="0.13058204"
+                    d="m 113.58878,114.51581 -27.119561,-27.372237 38.250371,4.649671 17.65215,-34.250727 7.39791,37.815095 38.02919,6.204128 -33.67821,18.72134 5.85119,38.08509 -28.2122,-26.24467 -34.412952,17.33375 z"
+                    inkscape:randomized="2.0816682e-17"
+                    inkscape:rounded="0"
+                    inkscape:flatsided="false"
+                    sodipodi:arg2="3.5970929"
+                    sodipodi:arg1="2.9687744"
+                    sodipodi:r2="53.808716"
+                    sodipodi:r1="21.523485"
+                    sodipodi:cy="110.81465"
+                    sodipodi:cx="134.79166"
+                    sodipodi:sides="5"
+                    id="path4570"
+                    style="opacity:1;fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:187.99578857;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:fill markers stroke"
+                    sodipodi:type="star" />
+                  <circle
+                    r="15.501633"
+                    cy="122.20571"
+                    cx="99.958801"
+                    id="path4574"
+                    style="opacity:1;fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:64.2519989;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:fill markers stroke" />
+                  <path
+                    transform="translate(4.8108515,36.348656)"
+                    inkscape:transform-center-y="-4.6753086"
+                    inkscape:transform-center-x="2.1074594e-06"
+                    d="m 154.64136,100.01853 -32.39148,0 16.19574,-28.051842 z"
+                    inkscape:randomized="2.08167e-17"
+                    inkscape:rounded="0"
+                    inkscape:flatsided="true"
+                    sodipodi:arg2="1.5707963"
+                    sodipodi:arg1="0.52359878"
+                    sodipodi:r2="9.3506145"
+                    sodipodi:r1="18.701227"
+                    sodipodi:cy="90.667915"
+                    sodipodi:cx="138.44562"
+                    sodipodi:sides="3"
+                    id="path4580"
+                    style="opacity:1;fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:64.2519989;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:fill markers stroke"
+                    sodipodi:type="star" />
+                  <rect
+                    y="66.353317"
+                    x="129.51804"
+                    height="29.399645"
+                    width="29.399645"
+                    id="rect4584"
+                    style="opacity:1;fill:#ba0303;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:10.00000095;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal" />
+                  <path
+                    inkscape:connector-curvature="0"
+                    id="rect4586"
+                    d="m 129.51792,66.353317 29.39976,24.669295 V 66.353317 Z"
+                    style="opacity:1;fill:#891500;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:10;stroke-linecap:butt;stroke-linejoin:bevel;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal" />
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </section>
+    <top-menu
+      :menuItems="menuItems"
+    />
+  </div>
+</template>
+
+<script>
+import TopMenu from '@/components/TopMenu'
+import RedisMenu from '@/assets/json/redis_menu.json'
+
+export default {
+  components: {
+    TopMenu
+  },
+  data: () => ({
+    menuItems: Object.values(RedisMenu)
+  }),
+  head: () => {
+    return {
+      title: "Redis",
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: "RedisはNoSQLの一種で、キー・バリュー型のインメモリデータベースです。メモリ上にデータを保存することができます。そのためRDBと違い高速に処理することができます。"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@keyframes FirstLayerAnimation {
+  0% {
+    opacity: 0;
+    transform: translate(120px, -10px);
+  }
+  10% {
+    opacity: 0;
+    transform: translate(120px, -10px);
+  }
+  20% {
+    opacity: 1;
+    transform: translate(-28px, 68px);
+  }
+  60% {
+    opacity: 1;
+    transform: translate(-28px, 68px);
+  }
+  70% {
+    opacity: 0;
+    transform: translate(-180px, -30px);
+  }
+  100% {
+    opacity: 0;
+    transform: translate(-180px, -30px);
+  }
+}
+@keyframes SecondLayerAnimation {
+  0% {
+    opacity: 0;
+    transform: translate(120px, 60px);
+  }
+  20% {
+    opacity: 0;
+    transform: translate(120px, 60px);
+  }
+  30% {
+    opacity: 1;
+    transform: translate(-28px, -34px);
+  }
+  70% {
+    opacity: 1;
+    transform: translate(-28px, -34px);
+  }
+  80% {
+    opacity: 0;
+    transform: translate(-190px, 60px);
+  }
+  100% {
+    opacity: 0;
+    transform: translate(-190px, 60px);
+  }
+}
+@keyframes ThirdLayerAnimation {
+  0% {
+    opacity: 0;
+    transform: translate(-260px, 120px);
+  }
+  30% {
+    opacity: 0;
+    transform: translate(-260px, 120px);
+  }
+  40% {
+    opacity: 1;
+    transform: translate(-28px, -8px);
+  }
+  80% {
+    opacity: 1;
+    transform: translate(-28px, -8px);
+  }
+  90% {
+    opacity: 0;
+    transform: translate(200px, -140px);
+  }
+  100% {
+    opacity: 0;
+    transform: translate(200px, -140px);
+  }
+}
+#g4680 {
+  animation-name: FirstLayerAnimation;
+  animation-duration: 10s;
+  animation-iteration-count: infinite;
+}
+#g4688 {
+  animation-name: SecondLayerAnimation;
+  animation-duration: 10s;
+  animation-iteration-count: infinite;
+}
+#g4702 {
+  animation-name: ThirdLayerAnimation;
+  animation-duration: 10s;
+  animation-iteration-count: infinite;
+}
+</style>
---
+++ pages/redis/setup.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="wrapper">
+    <article id="main">
+      <section>
+        <div v-html="SetupMd"></div>
+        <article-timestamp createdAt="2019/04/27" updatedAt="2019/04/27"/>
+      </section>
+    </article>
+    <side-menu :menuSections="menuSections"/>
+    <table-content :articleMd="SetupMd"/>
+  </div>
+</template>
+
+<script>
+import ArticleTimestamp from '@/components/ArticleTimestamp'
+import SideMenu from '@/components/SideMenu'
+import TableContent from '@/components/TableContent'
+import RedisMenu from '@/assets/json/redis_menu.json'
+import SetupMd from '@/static/markdown/redis/setup.md'
+
+export default {
+  components: {
+    ArticleTimestamp,
+    SideMenu,
+    TableContent
+  },
+  data: () => ({
+    menuSections: Object.values(RedisMenu)
+  }),
+  computed: {
+    SetupMd() {
+      return SetupMd
+    }
+  },
+  head: () => {
+    return {
+      title: RedisMenu.setup.headerTitle,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: RedisMenu.setup.headerDescription
+        }
+      ]
+    }
+  }
+}
+</script>
